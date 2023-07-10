3881146dc14eeac20b89fd2aa38976f83d14beb1
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-10 17:10:31 +0300

    modif color

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,5 @@
 const txt_2 = new Typed('.txt_2',{
-    strings: ["an Autodidacte","Font-end","Malagasy"],
+    strings: ["un DEV front-end","autodidacte","Malagasy"],
     typedSpeed: 200,
     backSpeed: 200,
     backDelay: 200,
