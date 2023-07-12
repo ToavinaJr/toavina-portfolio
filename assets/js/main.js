e4c4e15aa64f68177e0b31b543b2c4a95b75df5c
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-12 08:14:56 +0300

    toggle for dark modde

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,17 @@
-const txt_2 = new Typed('.txt_2',{
-    strings: ["un DEV Front-end,","un autodidacte,","Malagasy."],
-    typedSpeed: 200,
-    backSpeed: 200,
-    backDelay: 200,
-    loop: true
+// const txt_2 = new Typed('.txt_2',{
+//     strings: ["un DEV Front-end,","un autodidacte,","Malagasy."],
+//     typedSpeed: 200,
+//     backSpeed: 200,
+//     backDelay: 200,
+//     loop: true
+// })
+
+let modeSombre = true
+let toggle = document.getElementById('toggle')
+
+
+toggle.addEventListener('click', function(){
+    let body = document.querySelector('body')
+    body.classList.remove('bg-light')
+    body.classList.add('bg-dark')
 })
