303eca0d47bd04aa46f912034f5c4a902ae76ab5
Toavina Sylvianno <120978827+ToavinaJr@users.noreply.github.com>
2023-07-26 21:28:10 +0300

    integration via js

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,22 +4,17 @@ function addClassLists(div, classList) {
     }
 }
 
-// const txt_2 = new Typed('.txt_2',{
-//     strings: ["un DEV Front-end,","un autodidacte,","Malagasy."],
-//     typedSpeed: 200,
-//     backSpeed: 200,
-//     backDelay: 200,
-//     loop: true
-// })
+/************************************** */
 
 let toavina = {
     header : {
         logo: {
             classList : ["logo", "neo-light", "container"],
-            texts: ["Toavina", "Jr"]
+            texts: ["Toavina", "Jr"],
+            id: "logo"
         },
         img : {
-            source: "./assets/images/menu-icon.png",
+            src: "./assets/images/menu-icon.png",
             alt: "image non trouvé",
             id: "menu-toggle",
             classList : ["bg-light", "neo-light", "container"]
@@ -42,9 +37,23 @@ let toavina = {
     },
     accueil : {
         classList : ["txt-size-3", "txt-apropos", "bg-light", "neo-light", "container"],
-        // texts :  ["Bienvenu sur mon portfolio", 'Mon nom est <s cl', 'Je suis <span class="txt_2 txt-primary"></span>']
+        texts :  [
+                    '<div class="txt-size-3">Bienvenu sur mon portfolio</div>',
+                    '<div class="txt-size-3">Mon nom est<span class="txt-primary">Toavina Sylvianno</span></div>',
+                    '<div class="txt-size-3">Je suis <span class="txt_2 txt-primary"></span></div>'
+                ],
         container : {
             classList : ["text-container", "bg-light", "neo-light", "container"]
+        }
+    },
+
+    apropos : {
+        texts : ['<div class="txt-size-3 txt-apropos bg-light neo-light container"> Salut! Mon nom c\{est <span class="txt-primary">Toavina Sylvianno</span><br>Je suis en première année de master en <span class="txt-primary">Mathématiques et Informatique</span> à l\'université d\'Antananarivo <br>Actuellement, j\'apprends le <span class="txt-primary">Développement Web</span> chez <span class="txt-primary">Digital Training Center</span></div>']
+    },
+
+    contact : {
+        formContainer : {
+            classList : ["form-container", "bg-light", "txt-size-3", "neo-light", "container"],
         }
     }
 }
@@ -61,6 +70,7 @@ let header = document.getElementById('header')
 /* ***************************************** */
 
 let logo = document.createElement('div')
+logo.id = toavina.header.logo.id
 
 for ( item of toavina.header.logo.classList){
     logo.classList.add(item)
@@ -71,12 +81,12 @@ for ( text of toavina.header.logo.texts)
 
 /* ****************************************** */
 
-let menu = document.createElement('img')
+let menuBtn = document.createElement('img')
 
-addClassLists(menu, toavina.header.img.classList)
-menu.src = toavina.header.img.source
-menu.id = toavina.header.img.id
-menu.alt = toavina.header.img.alt
+addClassLists(menuBtn, toavina.header.img.classList)
+menuBtn.src = toavina.header.img.src
+menuBtn.id = toavina.header.img.id
+menuBtn.alt = toavina.header.img.alt
 
 /* ****************************************** */
 
@@ -86,16 +96,15 @@ addClassLists(nav, toavina.header.nav.classList)
 let navList = document.createElement('ul')
 addClassLists(navList, toavina.header.nav.navList.classList)
 
-let texts = toavina.header.nav.navList.navItem.texts;
-for (let i=0; i < texts.length; ++i) {
+for (let i=0; i < toavina.header.nav.navList.navItem.texts.length; ++i) {
     let navItem = document.createElement('li')
     addClassLists(navItem, toavina.header.nav.navList.navItem.classList)
 
     let navLink = document.createElement('a')
     addClassLists(navLink, toavina.header.nav.navList.navItem.navLink.classList)
 
-    navLink.textContent = texts[i]
-    navLink.href = "#" + texts[i]
+    navLink.textContent = toavina.header.nav.navList.navItem.texts[i]
+    navLink.href = "#" + toavina.header.nav.navList.navItem.texts[i]
 
     navItem.append(navLink)
     navList.append(navItem)
@@ -106,15 +115,87 @@ nav.append(navList)
 /* ****************************************** */
 
 header.append(logo)
-header.append(menu)
+header.append(menuBtn)
 header.append(nav)
 
 /* ****************************************** */
 
 let spanList= document.querySelectorAll('.logo span')
-for (item of spanList) {
-    addClassLists(item, ["txt-primary"])
+
+addClassLists(spanList[0], ["txt-primary"])
+addClassLists(spanList[1], ["txt-dark"])
+
+/* *******************************************
+*************       M A I N      *************
+******************************************* */
+
+let main = document.querySelector('main')
+
+let accueil = document.createElement('section')
+
+accueil.id = "accueil"
+addClassLists(accueil, toavina.accueil.classList)
+
+let accueilContainer = document.createElement('div')
+addClassLists(accueilContainer, toavina.accueil.container.classList )
+
+for (item of toavina.accueil.texts){
+    accueilContainer.innerHTML += item
 }
+
+accueil.append(accueilContainer)
+
+/******************************************* */
+
+let apropos = document.createElement('section')
+apropos.id = 'apropos'
+
+apropos.innerHTML += toavina.apropos.texts
+
+/******************************************* */
+
+let contact = document.createElement('section')
+contact.id = 'contact'
+let formContainer = document.createElement('div')
+addClassLists(formContainer, toavina.contact.formContainer.classList)
+
+formContainer.innerHTML += '<h2 class="txt-primary">Me contactez</h2>'
+let form = document.createElement('form')
+let inputContainer_1 = document.createElement('div')
+inputContainer_1.classList.add('input-container')
+
+inputContainer_1.innerHTML = '<input type="text" placeholder="Votre nom" required>'
+inputContainer_1.innerHTML += '<input type="email" placeholder="Votre email" required>'
+
+let inputContainer_2 = document.createElement('div')
+inputContainer_2.classList.add('input-container')
+inputContainer_2.innerHTML = '<input type="number" placeholder="Votre telephone">'
+inputContainer_2.innerHTML += '<input type="text" placeholder="Objet de votre mail" required>'
+
+form.append(inputContainer_1)
+form.append(inputContainer_2)
+
+form.innerHTML += '<textarea name="" id="" placeholder="Votre message" required></textarea>'
+form.innerHTML += '<input class="txt-size-2 txt-light" type="submit" value="Envoyer">'
+formContainer.append(form)
+contact.append(formContainer)
+
+main.append(accueil)
+main.append(apropos)
+main.append(contact)
+let menu_toggle = document.getElementById('menu-toggle')
+menu_toggle.addEventListener('click', () =>{
+    let nav = document.querySelector('nav')
+    nav.classList.toggle('active')
+})
+
+const txt_2 = new Typed('.txt_2',{
+    strings: ["un DEV Front-end,","un autodidacte,","Malagasy."],
+    typedSpeed: 200,
+    backSpeed: 200,
+    backDelay: 200,
+    loop: true
+})
 
 toggle.addEventListener('click', function(){
     let containers = document.querySelectorAll('.container')
@@ -134,26 +215,4 @@ toggle.addEventListener('click', function(){
         });
         modeSombre = true
     }
-})
-
-
-/* *******************************************
-*************       M A I N      *************
-******************************************* */
-
-let main = document.querySelector('main')
-
-let accueil = document.getElementById('accueil')
-addClassLists(accueil, toavina.accueil.classList)
-let accueilContainer = document.createElement('div')
-addClassLists(accueilContainer, toavina.accueil.container.classList )
-
-accueil.append(accueilContainer)
-main.append(accueil)
-
-let menu_toggle = document.getElementById('menu-toggle')
-menu_toggle.addEventListener('click', () =>{
-    let nav = document.querySelector('nav')
-    nav.classList.toggle('active')
-    console.log('click')
 })
